167ef7712337eadf6e683bb0b458e03cb07363db
Marco Lattuada <marco.lattuada@polimi.it>
2017-10-17 13:53:25 +0200

    Updated tests with stage

--- ExampleScripts/call_dagsim_stage.sh
+++ ExampleScripts/call_dagsim_stage.sh
@@ -1,1 +1,1 @@
-curl localhost:8080/bigsea/rest/ws/dagsim/15/2/8G/1000/application_1483347394756_0/J4S7/1483230000000
+curl localhost:8080/bigsea/rest/ws/dagsim/15/2/8G/1000/application_1483347394756_0/J4S7/1483234750000
